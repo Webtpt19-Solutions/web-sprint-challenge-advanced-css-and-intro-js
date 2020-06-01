71dd72355be5e77511d711c100e0f919eb564dfb
Chris Engel <Chris.Engel@yahoo.com>
2020-06-01 19:47:12 -0400

    done

--- index.js
+++ index.js
@@ -207,10 +207,16 @@ const artists = [
 
 (1) Name of the first artist (0th index) in the array
 (2) Bio of the third artist (2nd index) in the array */
-
+console.log(artists[0].name)
+console.log(artists[2].bio)
 
 
 /* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
+
+const fixed = artists[8]; // this is kind of a wierd question for 
+fixed.name = "Vincent Van Gogh";
+artists.splice(8, 1, fixed);
+console.log('task 2', artists[8])
 
 
 
@@ -222,20 +228,28 @@ const artists = [
  * For example, if getArtistByIndex is invoked with the artists dataset and the number 0,
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
-function getArtistByIndex(id, name) {
-    /* code here */
-  }
-  
+function getArtistByIndex(array, index) { // WHAT ARE THESE PARAMETER NAMES "(id, name)"", THIS IS EVEN CONFUSING TO ME!! 
+    return `The artist at index ${index} is ${array[index].name}.`
+};
+  console.log('task 3', getArtistByIndex(artists, 0))
   /**
 
 
 /* Task 4: Create a function called get20s() that takes data as an argument and returns an array with names of artists who were born the 20th century (1900-2000) */
 
-function get20s(/* Code here */){
+function get20s(data){
+    const twentiethCentrury = []
 
-  /* Code here */
-
+    for(let i = 0; i < data.length; i++){
+      const birthYear = data[i].years.split(" ")[0]
+      if(birthYear > 1900 && birthYear < 2000){
+        twentiethCentrury.push(data[i].name)
+      }
+    }
+    
+    return twentiethCentrury
 }
+console.log('Task 4', get20s(artists))
 
 
 /* Task 5: Create a function called `removeArtist` that takes two arguments:
@@ -248,14 +262,13 @@ function get20s(/* Code here */){
  * 
  * Note that sucessfully invoking this function multiple times without refreshing your browser will continuously remove artists from the array until there are none left. If you refresh your browser, the data will reset.  
 */
-function removeArtist(/*code here*/) {
-    /* code here */
-  }
-  
+function removeArtist(arr, index) {
+    const removed = arr.splice(index, 1)
+
+    return removed[0].name
+  };
+  console.log('Task 5', removeArtist(artists, 0))
   /**
-
-
-
 
 /* Task 6: Create a function called `addArtist` that can accept an array of information and add it to the artists array. Then, add a 21st artist to the array (you) with custom information!👩‍🎨👨‍🎨
 
@@ -269,11 +282,20 @@ bio: Add 1-2 sentences (or use lorem ipsum)
 
 At the end, this function should console.log() the new array with information added"*/
 
-function addArtist(/* Code here */){
+const newArtist = [{
+  id: 21,
+  name: "Chris Engel", 
+  years: 1992,
+  genre: "Web Design", 
+  nationality: "United State",
+  bio: "blah blah random stuff"
+}]
 
-    /* Code here */
-
-  }
+function addArtist(arr, arrData){ // The phrasing of this question is confusing
+  arr.push(arrData[0])
+  };
+  addArtist(artists, newArtist);
+  console.log("task 6", artists[artists.length - 1])
 
 /* Task 7: Create a function called lotsOfArt() that takes one argument: 
 
@@ -283,12 +305,17 @@ and returns an array with names of artists who painted more than 100 paintings.
 
 For example lotsOfArt(artists); will return ["Amedeo Modigliani", "Rene Magritte", ..."Albrecht Dürer"]*/
 
-function lotsOfArt(/* Code here */){
+function lotsOfArt(arr){
 
-  /* Code here */
-
+  const allThePaintings = [];
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i].paintings > 100){
+      allThePaintings.push(arr[i].name)
+    }
+  }
+  return allThePaintings
 }
-
+console.log('task 7', lotsOfArt(artists))
 
 
 // 🎨🎨 STRETCH 🎨🎨//
@@ -314,11 +341,22 @@ The function should console.log 50 chunks of HTML code that match the structure 
 
 ‼️ You do **NOT** need to get these to display on your page, but you can copy and paste the result into your HTML file if you'd like to see what that would look like. */
 
-function getHTML(/* Code here */){
-
-    /* Code here */
-
-  }
+// function getHTML(data){ // Why include an image for vangoph when there is no image in the artists array? This could confuse students
+//   for(let i in data){
+//     console.log(`
+//       <div id="artist">
+//       <div class="image">
+//           <img src="https://images.fineartamerica.com/images/artworkimages/mediumlarge/3/starry-night-by-vincent-van-gogh-vincent-van-gogh.jpg"/>
+//       </div>
+//       <div class = "name">
+//         <a href=${data[i].wikipedia}> ${data[i].name}</a>
+//       </div>
+//       <div class = "bio">${data[i].bio}.</div>
+//       </div>
+//     `)
+//   }
+// };
+// console.log('stretch', getHTML(artists))
 
 
 /* STRETCH 2: Create a function called `randomize` that takes a data array as an argument and returns a the same array in a randomized order. */
